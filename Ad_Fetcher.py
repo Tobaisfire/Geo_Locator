80c1cb472875285f944b3436d67ab7595257629e
Tobaisfire <kevalsaud25@gmail.com>
2023-01-25 18:07:31 +0530

    v 1.6

--- Ad_Fetcher.py
+++ Ad_Fetcher.py
@@ -49,7 +49,7 @@ try:
             elif (uploaded_file.name).split('.')[1] == 'xlsx':
                 dataframe = pd.read_excel(uploaded_file)
             else:
-                st.error('Wrong File extension plese provide :1 csv or :2 xlsx', icon="🚨")
+                st.error('Wrong File extension please provide :1 csv or :2 xlsx', icon="🚨")
 
 
         number = st.text_input('Cursor')
@@ -68,94 +68,96 @@ try:
     
     dt_string = now.strftime("%d/%m/%Y %H:%M")
     if button1:
-
-        if st.session_state['button'] == True:
-            try:
-                st.write('Start time:       ',dt_string)
-
-
-
-                tab1, tab2, tab3 = st.tabs(["Counting","Scheduler", "Result"])
-                if number !='':
-                    dataframe = dataframe.iloc[int(number)+1:]
-                else:
-                    dataframe = dataframe
-                with col2:
-                    with tab2:
-                        st.dataframe(dataframe)
-                        print('-------------------Program running-----------------------------------------------------------------------------')
-                        store= []
-
-                        recordm = dataframe
-                        lat = recordm['Latitude'].to_list()
-                        lon = recordm['Longitude'].to_list()
-                        id = recordm['Sublocationid'].to_list()
-                        locality = recordm['Beats LocalityName'].to_list()
-                        counter = 0
-                        for i in range(len(recordm)):
-
-                        
-
-                            lat2 = deg_to_dms(lat[i],pretty_print='latitude')
-
-                            long2 = deg_to_dms(lon[i],pretty_print='longitude')
-
-
-                            header = {'content-type':
-                                	'application/json'}
-
-                            url = f'https://www.google.com/maps/place/{lat2}+{long2}/'
-                            response = requests.get(url,headers = header)
-
-                            soup = BeautifulSoup(response.text)
-                            meta1 = soup.findAll('meta')
-                            meta = soup.find(itemprop="name")
-                            print(meta['content'].split('·')[1].strip())
-                            store = (meta['content'].split('·')[1].strip())
+        if uploaded_file is None:
+            st.error('Scheduler is not given please provide :1 csv or :2 xlsx', icon="🚨")
+        else:
+            if st.session_state['button'] == True:
+                try:
+                    st.write('Start time:       ',dt_string)
 
 
 
-                            tab3 = tab3.empty()
-                            data = pd.DataFrame({'Sublocationid':[id[i]],'Beatslocatliy':[locality[i]],'lat':[lat[i]],'lon':[lon[i]],'Address':[store]})
-                            df = df.append(data)
-                            tab1 = tab1.empty()
-                            with tab1:
-                                    counter+=1
-                                    st.write('Total count: ',len(dataframe),'       out of which        ',counter,' Row Completed')
-                            with tab3:
-                                    col1,col2 = st.columns([3,1])
-                                    with col1:
-                                        st.dataframe(df)
+                    tab1, tab2, tab3 = st.tabs(["Counting","Scheduler", "Result"])
+                    if number !='':
+                        dataframe = dataframe.iloc[int(number)+1:]
+                    else:
+                        dataframe = dataframe
+                    with col2:
+                        with tab2:
+                            st.dataframe(dataframe)
+                            print('-------------------Program running-----------------------------------------------------------------------------')
+                            store= []
 
-                                    with col2:
-                                            if len(dataframe) == len(df):
-                                                def convert_df(df):
-                                                
-                                                    return df.to_csv().encode('utf-8')
+                            recordm = dataframe
+                            lat = recordm['Latitude'].to_list()
+                            lon = recordm['Longitude'].to_list()
+                            id = recordm['Sublocationid'].to_list()
+                            locality = recordm['Beats LocalityName'].to_list()
+                            counter = 0
+                            for i in range(len(recordm)):
 
-                                                csv = convert_df(df)
+                            
 
-                                                st.download_button(
-                                                label="Download data as CSV",
-                                                data=csv,
-                                                file_name='Loaction.csv',
-                                                mime='text/csv',
-                                                        on_click=call_back)
-                if len(df) == len(dataframe):
-                    st.write('End time:       ',dt_string)          
-            except Exception as e:
-                if len(df) >= 1:
-                    def convert_df(df):
-                        return df.to_csv().encode('utf-8')
+                                lat2 = deg_to_dms(lat[i],pretty_print='latitude')
 
-                    csv = convert_df(df)
+                                long2 = deg_to_dms(lon[i],pretty_print='longitude')
 
-                    st.download_button(
-                    label="Download data as CSV",
-                    data=csv,
-                    file_name='location.csv',
-                    mime='text/csv',
-                    on_click=call_back)
+
+                                header = {'content-type':
+                                    	'application/json'}
+
+                                url = f'https://www.google.com/maps/place/{lat2}+{long2}/'
+                                response = requests.get(url,headers = header)
+
+                                soup = BeautifulSoup(response.text)
+                                meta1 = soup.findAll('meta')
+                                meta = soup.find(itemprop="name")
+                                print(meta['content'].split('·')[1].strip())
+                                store = (meta['content'].split('·')[1].strip())
+
+
+
+                                tab3 = tab3.empty()
+                                data = pd.DataFrame({'Sublocationid':[id[i]],'Beatslocatliy':[locality[i]],'lat':[lat[i]],'lon':[lon[i]],'Address':[store]})
+                                df = df.append(data)
+                                tab1 = tab1.empty()
+                                with tab1:
+                                        counter+=1
+                                        st.write('Total count: ',len(dataframe),'       out of which        ',counter,' Row Completed')
+                                with tab3:
+                                        col1,col2 = st.columns([3,1])
+                                        with col1:
+                                            st.dataframe(df)
+
+                                        with col2:
+                                                if len(dataframe) == len(df):
+                                                    def convert_df(df):
+                                                    
+                                                        return df.to_csv().encode('utf-8')
+
+                                                    csv = convert_df(df)
+
+                                                    st.download_button(
+                                                    label="Download data as CSV",
+                                                    data=csv,
+                                                    file_name='Loaction.csv',
+                                                    mime='text/csv',
+                                                            on_click=call_back)
+                    if len(df) == len(dataframe):
+                        st.write('End time:       ',dt_string)          
+                except Exception as e:
+                    if len(df) >= 1:
+                        def convert_df(df):
+                            return df.to_csv().encode('utf-8')
+
+                        csv = convert_df(df)
+
+                        st.download_button(
+                        label="Download data as CSV",
+                        data=csv,
+                        file_name='location.csv',
+                        mime='text/csv',
+                        on_click=call_back)
 
 except Exception as e:
     print(f"Current Error is:- {e}")
